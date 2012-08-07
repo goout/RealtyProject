86924b1adcef16c10da4f8008af0a5dcfc34c26a
Hleb <goout@bk.ru>
2012-08-07 10:35:09 +0300

    UserJdbcDAO.java update

--- Parent/RealtyService/src/main/java/org/realty/UserJdbcDAO.java
+++ Parent/RealtyService/src/main/java/org/realty/UserJdbcDAO.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserJdbcDAO {
 
@@ -53,4 +55,113 @@ public class UserJdbcDAO {
 
 	}
 
+	public void update(User user) {
+
+		try {
+			String querystring = "UPDATE User SET name=?,password=?,phone_number=?";
+			con = getConnection();
+			ptmt = con.prepareStatement(querystring);
+			ptmt.setString(1, user.getname());
+			ptmt.setString(2, user.getpassword());
+			ptmt.setString(3, user.getphoneNumber());
+			ptmt.executeUpdate();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (ptmt != null)
+					ptmt.close();
+				if (con != null)
+					con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+		}
+
+	}
+
+	public void delete(int userId) {
+
+		try {
+			String querystring = "DELETE FROM User WHERE user_id=?";
+			con = getConnection();
+			ptmt = con.prepareStatement(querystring);
+			ptmt.setInt(1, userId);
+			ptmt.executeUpdate();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (ptmt != null)
+					ptmt.close();
+				if (con != null)
+					con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+		}
+
+	}
+	
+	
+	 public List findAll()
+	  {
+	   List users=new ArrayList();
+	   User user=null;
+	   try
+	   {
+	    String querystring="SELECT * FROM User";
+	    con=getConnection();
+	    ptmt=con.prepareStatement(querystring);
+	    rs=ptmt.executeQuery();
+	    while(rs.next())
+	    {
+	     user=new User();
+	     user.setuserId(rs.getInt(1));
+	     user.setname(rs.getString(2));
+	     user.setpassword(rs.getString(3));
+	     user.setphoneNumber(rs.getString(4));
+	     
+	     users.add(user);
+	    }
+	   }
+	   catch(SQLException e)
+	   {
+	    e.printStackTrace();
+	   }
+	   finally
+	   {
+	    try
+	    {
+	    if(rs!=null)
+	        rs.close();
+	    if(ptmt!=null)
+	       ptmt.close();
+	    if(con!=null)
+	       con.close();
+	    }
+	    catch(SQLException e)
+	    {
+	     e.printStackTrace();
+	    }
+	    catch(Exception e)
+	    {
+	     e.printStackTrace();
+	    }
+	    
+	   }
+	   return users;
+	  }
 }
