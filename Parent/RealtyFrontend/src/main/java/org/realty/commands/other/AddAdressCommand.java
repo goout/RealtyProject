e69d51cca5106b64e920abe42031b11a10cd1400
Hleb <goout@bk.ru>
2012-09-04 14:34:55 +0300

    AddAdressCommand.java

--- Parent/RealtyFrontend/src/main/java/org/realty/commands/other/AddAdressCommand.java
+++ Parent/RealtyFrontend/src/main/java/org/realty/commands/other/AddAdressCommand.java
@@ -1,24 +1,20 @@
 package org.realty.commands.other;
 
 
-import org.realty.UsrInfo;
+
 import org.realty.commands.Command;
-import org.realty.dao.AdvertJdbcDAO;
+
 import org.realty.dao.CityJdbcDAO;
-import org.realty.dao.CommentJdbcDAO;
-import org.realty.dao.UserJdbcDAO;
-import org.realty.dto.CommentUserAdvertDTO;
-import org.realty.entity.Advert;
+
 import org.realty.entity.City;
-import org.realty.entity.Comment;
-import org.realty.entity.User;
+
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+
 import java.io.IOException;
-import java.util.ArrayList;
+
 import java.util.List;
 
 public class AddAdressCommand implements Command {
